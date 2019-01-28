3e410f092f03e68d73da8127f4fe9bf2b358a88b
Dirk Hoffmann (Dell EMC) <Dirk.Hoffmann@dell.com>
2019-01-28 18:04:30 +0100

    minimal changes

--- ci/compile.sh
+++ ci/compile.sh
@@ -1,44 +1,49 @@
 #!/bin/bash
 
+set -e
+
 cd https-only-tests-master
 export ROOT_FOLDER=$(pwd)
 
 function main() {
     echo "../artifactory"
-    ls -lsh ../artifactory
+    ls -lsha ../artifactory
     echo ""
 
     echo "https-only-tests-master:"
-    ls -lsh
+    ls -lsha
 
     echo ""
     echo "targetEnv=${TARGET_ENV}"
     echo ""
-    ./mvnw package
+    # ./mvnw package
+    ./mvnw install
 
 
     echo "https-only-tests-master:"
-    ls -lsh
+    ls -lsha
     echo "https-only-tests-master/target:"
-    ls -lsh target
+    ls -lsha target
 
     # convention: output-<repoName>-<taskname>
-    cp -r target output-https-only-tests-master-compile
+    cp -rT ../https-only-tests-master output-https-only-tests-master-compile
     echo "output-https-only-tests-master-compile:"
-    ls -lsh output-https-only-tests-master-compile
+    ls -lsha output-https-only-tests-master-compile
 }
  
+mkdir "${HOME}/.m2"
 M2_HOME="${HOME}/.m2"
 M2_CACHE="${ROOT_FOLDER}/maven"
+mkdir "${HOME}/.gradle"
 GRADLE_HOME="${HOME}/.gradle"
 GRADLE_CACHE="${ROOT_FOLDER}/gradle"
  
 echo "Generating symbolic links for caches"
- 
+set -x
 [[ -d "${M2_CACHE}" && ! -d "${M2_HOME}" ]] && ln -s "${M2_CACHE}" "${M2_HOME}"
 [[ -d "${GRADLE_CACHE}" && ! -d "${GRADLE_HOME}" ]] && ln -s "${GRADLE_CACHE}" "${GRADLE_HOME}"
+set +x
 
-mkdir ~/.m2
 cat <<EOT > ~/.m2/settings.xml
 <?xml version='1.0' encoding='UTF-8'?>
 <settings xsi:schemaLocation='http://maven.apache.org/SETTINGS/1.0.0 http://maven.apache.org/xsd/settings-1.0.0.xsd'
