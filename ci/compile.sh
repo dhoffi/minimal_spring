c5f74f40300d6bc09a9202e3a750e4f1b1c20e21
Dirk Hoffmann (Dell EMC) <Dirk.Hoffmann@dell.com>
2019-01-28 13:05:42 +0100

    maven set jcentral in /Users/hoffmd9/.m2/settings.xml

--- ci/compile.sh
+++ ci/compile.sh
@@ -1,19 +1,58 @@
 #!/bin/bash
 
-echo "../artifactory"
-ls -lsh ../artifactory
-echo ""
+function main() {
+    echo "../artifactory"
+    ls -lsh ../artifactory
+    echo ""
 
-cd https-only-tests-master
-ls -lsh
+    cd https-only-tests-master
+    ls -lsh
 
-echo ""
-echo "targetEnv=${TARGET_ENV}"
-echo ""
-./mvnw package
+    echo ""
+    echo "targetEnv=${TARGET_ENV}"
+    echo ""
+    ./mvnw package
 
-# convention: output-<repoName>-<taskname>
+    # convention: output-<repoName>-<taskname>
 
-cp -r target output-https-only-tests-master-compile
+    cp -r target output-https-only-tests-master-compile
 
-ls -lsh output-https-only-tests-master-compile
+    ls -lsh output-https-only-tests-master-compile
+}
+
+main
+
+cat <<EOT > ~/.m2/settings.xml
+<?xml version='1.0' encoding='UTF-8'?>
+<settings xsi:schemaLocation='http://maven.apache.org/SETTINGS/1.0.0 http://maven.apache.org/xsd/settings-1.0.0.xsd'
+          xmlns='http://maven.apache.org/SETTINGS/1.0.0' xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance'>
+    <profiles>
+        <profile>
+            <repositories>
+                <repository>
+                    <snapshots>
+                        <enabled>false</enabled>
+                    </snapshots>
+                    <id>central</id>
+                    <name>bintray</name>
+                    <url>http://jcenter.bintray.com</url>
+                </repository>
+            </repositories>
+            <pluginRepositories>
+                <pluginRepository>
+                    <snapshots>
+                        <enabled>false</enabled>
+                    </snapshots>
+                    <id>central</id>
+                    <name>bintray-plugins</name>
+                    <url>http://jcenter.bintray.com</url>
+                </pluginRepository>
+            </pluginRepositories>
+            <id>bintray</id>
+        </profile>
+    </profiles>
+    <activeProfiles>
+        <activeProfile>bintray</activeProfile>
+    </activeProfiles>
+</settings>
+EOT
